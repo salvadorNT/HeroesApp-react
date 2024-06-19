e4e5f00c961f93cbf842e448d7e69a920518bd10
salvador <salvadorjnt@gmail.com>
2024-06-18 23:43:05 -0600

    created first routes and blank pages

---
+++ src/heroes/pages/DcPage.jsx
@@ -0,0 +1,5 @@
+export const DcPage = () => {
+    return (
+      <h1>DC Page</h1>
+    )
+  }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './styles.css'
-import { HeroesApp } from './HeroesApp.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css';
+import { HeroesApp } from './HeroesApp.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HeroesApp />
+    <BrowserRouter>
+      <HeroesApp />
+    </BrowserRouter>
   </React.StrictMode>,
 )
