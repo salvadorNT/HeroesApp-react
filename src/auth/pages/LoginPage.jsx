c861bbee23121a7938ee9a7050cc7fc44aace207
salvador <salvadorjnt@gmail.com>
2024-06-19 22:55:22 -0600

    created function to simulate login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,6 +1,24 @@
+import { useNavigate } from "react-router"
 
 export const LoginPage = () => {
+
+  const navigate = useNavigate()
+  const onLogin = () => {
+    navigate('/heroes/marvel')
+  }
+
   return (
-    <h1>LoginPage</h1>
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr />
+
+      <button
+        className="btn btn-primary"
+        onClick={onLogin}
+      >
+        Login
+      </button>
+
+    </div>
   )
 }
