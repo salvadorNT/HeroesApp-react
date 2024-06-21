80448a04bc47ebe66c78c94ea5159308e310bc3f
salvador <salvadorjnt@gmail.com>
2024-06-20 23:55:35 -0600

    created functionality of HeroPage

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,8 +1,43 @@
+import { Navigate, useNavigate, useParams } from "react-router"
+import { getHeroById } from "../helpers";
+import { useMemo } from "react";
 
 export const HeroPage = () => {
+
+  const navigate = useNavigate()
+
+  const {id} = useParams();
+  const hero = getHeroById( id );
+
+  const onNavigateBack = () => {
+    navigate(-1);
+  }
+
+  if( !hero ){
+    return <Navigate to="marvel"/>
+  }
+
+  const heroImageUrl = `/assets/heroes/${id}.jpg`;
+
+  
   return (
-    <>
-        <h1>HeroPAge</h1>
-    </>
+    <div className="row mt-5">
+        <div className="col-4">
+          <img className="img-thumbnail" src={heroImageUrl} alt={hero.superhero} />
+        </div>
+        <div className="col-8">
+          <h3>{hero.superhero}</h3>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item"><b>Alter ego:</b> {hero.alter_ego}</li>
+            <li className="list-group-item"><b>Publisher:</b> {hero.publisher}</li>
+            <li className="list-group-item"><b>First Appearence:</b> {hero.first_appearance}</li>
+          </ul>
+          <h5 className="mt-3">characters</h5>
+          <p>{hero.characters}</p>
+
+          <button className="btn btn-primary" onClick={onNavigateBack}>Regresar</button>
+
+        </div>
+    </div>
   )
 }
