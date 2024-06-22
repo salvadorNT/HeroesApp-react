1a6ef03646bd2eeb60ccfe07234a0f037ae1e77f
salvador <salvadorjnt@gmail.com>
2024-06-21 22:14:24 -0600

    public routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,13 +3,23 @@ import { Navigate, Route, Routes } from "react-router";
 import { HeroesRoutes } from "../heroes";
 import { LoginPage } from "../auth";
 import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 
 export const AppRouter = () => {
     return (
         <>
             <Routes>
-                <Route path="login" element={<LoginPage />} />
+
+
+                <Route path="login/*" element={
+                    <PublicRoute>
+                        {/* <LoginPage /> */}
+                        <Routes>
+                            <Route path="/*" element={<LoginPage />} />
+                        </Routes>
+                    </PublicRoute>
+                } />
 
                 <Route path="/*" element={
                     <PrivateRoute>
--- src/router/PublicRoute.jsx
+++ src/router/PublicRoute.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react"
+import { AuthContext } from "../auth"
+import { Navigate } from "react-router"
 
-export const PublicRoute = () => {
-  return (
-    <div>PublicRoute</div>
-  )
+export const PublicRoute = ({ children }) => {
+    const { logged } = useContext(AuthContext)
+
+    return (!logged) ? children : <Navigate to='/marvel' />
 }
