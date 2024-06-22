b32f263b8a74e411a1f2c185172c0c297a99410e
salvador <salvadorjnt@gmail.com>
2024-06-21 20:09:54 -0600

    simulate login an user

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,18 +1,42 @@
 import { useReducer } from "react"
 import { AuthContext } from "./AuthContext"
 import { authReducer } from "./authReducer"
+import { types } from "../types/types"
 
 
-const initialState = {
-    logged: false,
+const init = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    return {
+        logged: !!user,
+        user: user
+    }
+
+
 }
 
 export const AuthProvider = ({ children }) => {
 
-    const [state, dispatch] = useReducer(authReducer, initialState);
+    const [state, dispatch] = useReducer(authReducer, {}, init);
+
+    const onLogin = (id = 123, name = '') => {
+        const user = {
+            id: id,
+            name: name
+        }
+
+        const action = {
+            type: types.login,
+            payload: user
+        }
+
+        localStorage.setItem('user', JSON.stringify(user))
+
+        dispatch(action);
+    }
 
     return (
-        <AuthContext.Provider value={{}}>
+        <AuthContext.Provider value={{ ...state, login: onLogin }}>
             {children}
         </AuthContext.Provider>
     )
--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,9 +1,14 @@
+import { useContext } from "react"
 import { useNavigate } from "react-router"
+import { AuthContext } from "../context/AuthContext"
 
 export const LoginPage = () => {
 
-  const navigate = useNavigate()
+  const {login} = useContext(AuthContext);
+  const navigate = useNavigate();
+
   const onLogin = () => {
+    login(123, 'José Salvador');
     navigate('/heroes/marvel')
   }
 
