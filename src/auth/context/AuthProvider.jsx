de4d5f6c416058a63082ffe3713e280ed2b160be
salvador <salvadorjnt@gmail.com>
2024-06-21 20:37:44 -0600

    logout user erasing user data from local storage

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -20,23 +20,29 @@ export const AuthProvider = ({ children }) => {
     const [state, dispatch] = useReducer(authReducer, {}, init);
 
     const onLogin = (id = 123, name = '') => {
-        const user = {
-            id: id,
-            name: name
-        }
 
-        const action = {
-            type: types.login,
-            payload: user
-        }
+        const user = { id: id, name: name }
+        const action = { type: types.login, payload: user }
 
         localStorage.setItem('user', JSON.stringify(user))
 
         dispatch(action);
     }
 
+    const onLogout = () => {
+        localStorage.removeItem('user');
+
+        dispatch({ type: types.logout })
+    }
+
     return (
-        <AuthContext.Provider value={{ ...state, login: onLogin }}>
+        <AuthContext.Provider value={{
+            ...state,
+
+            // Methods
+            login: onLogin,
+            logout: onLogout
+        }}>
             {children}
         </AuthContext.Provider>
     )
