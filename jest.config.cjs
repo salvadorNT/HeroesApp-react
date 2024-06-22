0c39ccda08f9bae6e98bc32ea48f2c4b760f7894
salvador <salvadorjnt@gmail.com>
2024-06-22 17:26:40 -0600

    test configuration

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,3 +1,8 @@
+const esModules = [
+    'query-string',
+  ];
+
 module.exports = {
     testEnvironment: 'jest-environment-jsdom',
+    transformIgnorePatterns: ['node_modules/(?!(query-string|decode-uri-component|split-on-first|filter-obj)/)',],
 }
