51500d2126891454542e9be5529cee0716f9f6e7
salvador <salvadorjnt@gmail.com>
2024-06-21 20:10:33 -0600

    show user name in navbar

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,13 +1,16 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context/AuthContext';
 
 
 export const Navbar = () => {
 
-    const navigate = useNavigate()
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate();
 
     const onLogout = () => {
         navigate('/login', {
-            replace:true
+            replace: true
         });
     }
 
@@ -49,7 +52,7 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className='nav-item nav-link text-primary'>
-                        Salvador
+                        { user?.name }
                     </span>
                     <button
                         className='nav-item nav-link btn'
